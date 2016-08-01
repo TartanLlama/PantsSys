6b574e2775df5c64e127471327e45417abf46c75
Simon Brand <Simon Brand>
2016-08-01 18:05:28 +0100

    Diagnostics working

--- Panth/include/AST.hpp
+++ Panth/include/AST.hpp
@@ -1,39 +1,85 @@
+#pragma once
+
 #include <memory>
+#include <vector>
 
-class ASTNode{};
+namespace panth {
+	enum class BinOpSym {
+		Add, Sub, Mul, Div, Lt, Le, Gt, Ge, Eq
+	};
 
-using ASTNodeUP = std::unique_ptr<AstNode>;
+	enum class UnaryOpSym {
+		Min, Not
+	};
 
-class FuncDecl : public ASTNode
-{};
-class Quote : public ASTNode
-{};
-class For : public ASTNode
-{};
-class Operation : public ASTNode
-{};
+	class ASTNode {};
 
-class Let : public ASTNode
-{
+	using ASTNodeUP = std::unique_ptr<ASTNode>;
 
-};
+	class List : public ASTNode
+	{
+		std::vector<ASTNodeUP> m_elems;
+	};
 
-class Cond : public ASTNode
-{
-    std::vector<AstNodeUP> m_conds;
-};
+	class Id : public ASTNode {
+		std::string m_id;
+	};
+	class FuncDecl : public ASTNode
+	{
+		Id m_name;
+		List m_args;
+		ASTNodeUP m_body;
+	};
 
-class If : public ASTNode
-{
-private:
-    ASTNodeUP m_cond;
-    ASTNodeUP m_if;
-    ASTNodeUP m_else;
-};
+	class Quote : public ASTNode
+	{
+		ASTNodeUP m_form;
+	};
 
-class Call : public ASTNode
-{
-private:
-    FuncDecl& m_func;
-    std::vector<ASTNodeUP> m_args;
-};
+	class For : public ASTNode
+	{
+		Id m_id;
+		ASTNode m_list;
+		ASTNode m_body;
+	};
+
+	class BinaryOp : public ASTNode
+	{
+		ASTNodeUP m_lhs;
+		ASTNodeUP m_rhs;
+		BinOpSym m_op;
+	};
+
+	class Let : public ASTNode
+	{
+		ASTNodeUP m_lhs;
+		ASTNodeUP m_rhs;
+	};
+
+	class Cond : public ASTNode
+	{
+		std::vector<ASTNode> m_conds;
+	};
+
+	class If : public ASTNode
+	{
+	private:
+		ASTNodeUP m_cond;
+		ASTNodeUP m_if;
+		ASTNodeUP m_else;
+	};
+
+	class Call : public ASTNode
+	{
+	private:
+		FuncDecl& m_func;
+		std::vector<ASTNodeUP> m_args;
+	};
+
+	class AST {
+	public:
+		AST::AST(ASTNodeUP node) : m_node{ std::move(node) } {}
+	private:
+		ASTNodeUP m_node;
+	};
+}
