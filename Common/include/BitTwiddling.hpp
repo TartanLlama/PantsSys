25fdcaba737905f8597d00b2d7d2d14dde6c15a1
Simon Brand <simon@codeplay.com>
2016-11-29 19:43:57 +0000

    [PantsSys] Move bit twiddling to util header

---
+++ Common/include/BitTwiddling.hpp
@@ -0,0 +1,26 @@
+#pragma once
+#include <cstdint>
+
+namespace pants {
+    namespace util {
+        /// Extracts the high 16 bits from a 32 bit unsigned integer
+        inline uint16_t high16(uint32_t i) {
+            return i >> 16;
+        }
+
+        /// Extracts the low 16 bits from a 32 bit unsigned integer
+        inline uint16_t low16(uint32_t i) {
+            return i & 0x00ff;
+        }
+
+        /// Extracts the high 8 bits from a 16 bit unsigned integer
+        inline uint8_t high8(uint16_t i) {
+            return i >> 8;
+        }
+
+        /// Extracts the low 8 bits from a 16 bit unsigned integer
+        inline uint8_t low8(uint16_t i) {
+            return i & 0x0f;
+        }
+    }
+}
