e714c62a5b5bb5b9ee2075a486e82c8e99dd8f50
Simon Brand <tartanllama@gmail.com>
2016-11-29 18:07:14 +0000

    [PantsAsm] Add helloworld example, fix copy implementation

---
+++ PantsAsm/samples/helloworld.s
@@ -0,0 +1,54 @@
+        set me 0
+        set rs 19
+        store rs
+        set rs 1
+        add me me rs
+        copy rs me
+        set rs 11
+        store rs
+        set rs 1
+        add me me rs
+        copy rs me
+        set rs 37
+        store rs
+        set rs 1
+        add me me rs
+        copy rs me
+        set rs 37
+        store rs
+        set rs 1
+        add me me rs
+        copy rs me
+        set rs 47
+        store rs
+        set rs 1
+        add me me rs
+        copy rs me
+        set rs 9
+        store rs
+        set rs 1
+        add me me rs
+        copy rs me
+        set rs 83
+        store rs
+        set rs 1
+        add me me rs
+        copy rs me
+        set rs 47
+        store rs
+        set rs 1
+        add me me rs
+        copy rs me
+        set rs 61
+        store rs
+        set rs 1
+        add me me rs
+        copy rs me
+        set rs 37
+        store rs
+        set rs 1
+        add me me rs
+        copy rs me
+        set rs 7
+        store rs
+        halt
