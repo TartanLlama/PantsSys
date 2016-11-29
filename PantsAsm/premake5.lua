dc135aa41233a13897a9eaf4cd73b738df4f449e
Simon Brand <simon@codeplay.com>
2016-11-29 18:06:19 +0000

    [PantsAsm] Link against fmt

--- PantsAsm/premake5.lua
+++ PantsAsm/premake5.lua
@@ -3,5 +3,6 @@ project "PantsAsm"
     language "C++"
     flags {"C++14"}
     targetdir "../build/bin/%{cfg.buildcfg}"
-    includedirs { "../Common/include", "PantsAsm/include", "../ext/catch" }
+    includedirs { "../Common/include", "../ext/fmt", "PantsAsm/include", "../ext/catch" }
+    links {"fmt"}
     files { "src/**.cpp", "include/**.hpp", "../Common/include/**.hpp", "../Common/src/**.cpp"}
