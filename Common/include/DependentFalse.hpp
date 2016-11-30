03932e9727deb7a715f95017752ab627fb5ea9b8
Simon Brand <tartanllama@gmail.com>
2016-11-30 16:54:26 +0000

    [PantsSys] Add doc gen command

--- Common/include/DependentFalse.hpp
+++ Common/include/DependentFalse.hpp
@@ -2,4 +2,7 @@
 
 #include <type_traits>
 
+/// A dependent false template class for use in unconditional static_asserts
+///
+/// static_assert(DependentFalse<T>::value, "message");
 template <typename T> struct DependentFalse : std::false_type {};
