8416d7b7b9e4e549b4c6962b23a80c08d1fbbed9
Simon Brand <tartanllama@gmail.com>
2016-12-02 17:31:01 +0000

    [PantsSim] Fix SDL2 includes

--- PantsSim/include/PantsUI.hpp
+++ PantsSim/include/PantsUI.hpp
@@ -3,9 +3,17 @@
 #include <stdexcept>
 
 #define SDL_MAIN_HANDLED
+
+#ifdef __linux__
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
 #include <SDL2/SDL_ttf.h>
+#elif _WIN32
+#include <SDL.h>
+#include <SDL_image.h>
+#include <SDL_ttf.h>
+#endif
+
 
 #include "Config.hpp"
 #include <iostream>
