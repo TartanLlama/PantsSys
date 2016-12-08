47500181a04506aafb383ee95d8d96499f48be79
Simon Brand <tartanllama@gmail.com>
2016-12-08 17:00:45 +0000

    [Premake] Factor out dependency builds from main premake file

---
+++ ext/premake5.lua
@@ -0,0 +1,7 @@
+project "fmt"
+    kind "StaticLib"
+    language "C++"
+    flags {"C++14"}
+    targetdir "build/lib/%{cfg.buildcfg}"
+    includedirs {"fmt"}
+    files {"fmt/fmt/**.h", "fmt/fmt/**.cc"}
