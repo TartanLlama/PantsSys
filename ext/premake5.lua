650435109cc98313443ae260859fcdf8d9235386
Simon Brand <tartanllama@gmail.com>
2016-12-14 08:41:26 +0000

    [ext] Only use MSVC warning in Windows config

--- ext/premake5.lua
+++ ext/premake5.lua
@@ -4,6 +4,7 @@ project "fmt"
     flags {"C++14"}
     targetdir "build/lib/%{cfg.buildcfg}"
     includedirs {"fmt"}
-	disablewarnings {"4127"}
     files {"fmt/fmt/**.h", "fmt/fmt/**.cc"}
+    filter "platforms:Win64"
+    	disablewarnings {"4127"}
 
